36868fa6e9d1f216a0ebe022328fc958e0472e3e
Christophe Kafrouni <ckafrouni@proton.me>
2023-03-01 18:24:21 +0100

    Add scaffold for Fiche04

---
+++ Fiche04/requettes.sql
@@ -0,0 +1,85 @@
+-- 1.
+-- Pour quel(s) éditeur(s) a travaillé « Goscinny » ?
+
+-- 2.
+-- Quel est/sont le(s) dessinateur(s) de l'album « Astérix chez les Belges » ?
+
+-- 3.
+-- Quels sont les auteurs qui ont travaillé comme dessinateurs et/ou coloristes chez « Dupuis » ?
+
+-----------------------------
+-- Exercices sur 'GROUP BY'
+-----------------------------
+-- 4.
+-- Donnez, pour chaque scénariste, son identifiant, son nom et le nombre d’albums qu’il a écrits.
+
+-- 5.
+-- Pour chaque éditeur ayant publié au moins 10 albums, donnez son identifiant, son nom, son pays
+-- et la date du plus ancien et du plus récent de ses albums.
+
+-- 6.
+-- Donnez, pour chaque série, son nom ainsi que le nombre d’albums en faisant partie et le prix total
+-- à payer si on veut acheter tous les albums de la série. Classez le résultat par ordre décroissant du
+-- nombre d’albums. Vous ne devez pas prendre les séries sans albums.
+
+-- 7.
+-- Donnez, par année, le nombre d’albums édités en Belgique ainsi que le prix moyen de ces albums.
+-- Il ne faut garder que les années où il y a eu plusieurs albums édités en Belgique et il faut classer
+-- le résultat par ordre décroissant du nombre d’albums édités en Belgique et, en cas d’égalité, par
+-- ordre croissant de l’année.
+
+-----------------------------
+-- Exercices mélangés
+-----------------------------
+-- 8.
+-- Combien d'auteurs (différents) a-t-on répertoriés pour l'album « Coke en Stock » dont l’ISBN est
+-- 2-203-00109-0 ?
+
+-- 9.
+-- Donnez les isbn, titres et les prix des albums dessinés par « Uderzo » entre 1985 et 1995.
+
+-- 10.
+-- Donnez la liste des albums édités en Belgique ou en France, appartenant à une série et ayant
+-- plusieurs auteurs. Pour chacun de ces albums, affichez son ISBN, son titre, le nom de sa série, son
+-- prix et son nombre d’auteurs.
+
+-- 11.
+-- Donnez, pour chaque éditeur, son identifiant, son nom et le nombre de séries pour lesquelles il a
+-- publié au moins un album. Classez les résultats par ordre décroissant du nombre de séries, et pour
+-- les éditeurs ayant le même nombre de séries, par nom d’éditeur. Vous ne devez pas afficher les
+-- éditeurs n’ayant pas publié d’albums appartenant à une série.
+
+-- 12.
+-- Quels sont les albums édités par « Dupuis » pour lesquels on connaît le coloriste ?
+
+-- 13.
+-- Quelle est la date d’édition de l'album le plus récent ayant « Goscinny » parmi ses auteurs ?
+
+-- 14.
+-- Donnez les identifiants et noms des auteurs qui ont collaboré, d'une façon ou d'une autre, à des
+-- albums de la série « Astérix » avant 2010.
+
+-- 15.
+-- Quels rôles « Uderzo » a-t-il tenus dans les albums édités par « Dargaud » ?
+
+-- 16.
+-- Quels sont les auteurs qui ont joué plusieurs rôles (dessinateur, coloriste ...) dans un même
+-- album ?
+
+-- 17.
+-- Combien y a-t-il d'albums dessinés par chaque dessinateur ? Pour chaque dessinateur, affichez
+-- son identifiant, son nom et le nombre d’albums dessinés en classant le résultat par ordre
+-- décroissant du nombre d’albums dessinés
+
+-- 18.
+-- Affichez le nom des séries n’ayant eu qu’un seul auteur.
+
+-- 19.
+-- Donnez l’isbn, le titre et la date d’édition de tous les albums, édités en 1980 ou en 1981,
+-- n’appartenant pas à une série.
+
+-- 20.
+-- Pour chaque éditeur qui a édité au moins un album de moins de 10€, affichez son identifiant, son
+-- nom, sa nationalité, et le nombre d’albums de moins de 10€ qu'il a publiés. Les éditeurs de même
+-- nationalité doivent se suivre et pour une même nationalité, les éditeurs doivent être triés par
+-- ordre décroissant du nombre d'albums de moins de 10 euros.
