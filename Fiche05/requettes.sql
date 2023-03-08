90977e6ec374d0e6c8975dccd00ba0f597322914
Christophe Kafrouni <ckafrouni@proton.me>
2023-03-08 21:19:06 +0100

    Add scaffold for Fiche05

---
+++ Fiche05/requettes.sql
@@ -0,0 +1,107 @@
+------------------------------------------------------------------------------------------------------------------------
+-- Exercices avec subselect
+------------------------------------------------------------------------------------------------------------------------
+
+-- 1.
+-- Donnez, pour chaque album dont le prix est supérieur au prix moyen de tous les albums, son isbn,
+-- son titre, sa date d’édition, son prix et le nom de son éditeur. Triez le résultat par ordre décroissant
+-- de prix et pour un même prix, dans l’ordre chronologique.
+-- TODO 1
+
+-- 2.
+-- Donnez l’identifiant et le nom de tous les auteurs ayant été scénariste mais jamais coloriste ou
+-- dessinateur.
+-- TODO 2
+
+-- 3.
+-- Donnez les éditeurs qui ont publié au moins un album n’ayant pas d’auteurs connus.
+-- TODO 3
+
+-- 4.
+-- Donnez l’identifiant et le nom des auteurs ayant publié tous leurs albums chez "Dupuis". Il ne faut
+-- pas prendre les auteurs n’ayant participé à aucun album.
+-- TODO 4
+
+------------------------------------------------------------------------------------------------------------------------
+-- Exercices mélangés
+------------------------------------------------------------------------------------------------------------------------
+
+-- 5.
+-- Donnez les noms des séries dont au moins un album a été édité chez "Dupuis", et, pour chacune
+-- d’elles, le nombre d’albums édités chez "Dupuis".
+-- TODO 5
+
+-- 6.
+-- Donnez les éditeurs qui ont édité des albums en 1978 ?
+-- TODO 6
+
+-- 7.
+-- Quels sont les albums pour lesquels le coloriste n'a pas été spécifié ?
+-- TODO 7
+
+-- 8.
+-- Donnez, pour chaque série, son nom ainsi que le nombre d’auteurs y ayant contribué. Il ne faut
+-- garder que les séries pour lesquelles plusieurs auteurs ont contribué. Classez les résultats en ordre
+-- décroissant du nombre d’auteurs.
+-- TODO 8
+
+-- 9.
+-- Donnez, pour chaque scénariste, son identifiant, son nom et le nombre d’albums qu’il a écrits.
+-- TODO 9
+
+-- 10.
+-- Donnez, pour chaque auteur, son identifiant, son nom et le nombre d’albums auxquels il a
+-- participé ; affichez les résultats dans l’ordre décroissant du nombre d’albums. Les auteurs n’ayant
+-- participé à aucun album ne doivent pas apparaître.
+-- TODO 10
+
+-- 11.
+-- Quels sont les scénaristes dont on a édité, après 1990, des albums qui coûtent moins de 12 euros ?
+-- TODO 11
+
+-- 12.
+-- Donnez les isbn et titre du (des) album(s) le(s) moins cher(s) édités en 1976 ?
+-- TODO 12
+
+-- 13.
+-- Quels sont les albums qui n'ont qu'un seul auteur ?
+-- TODO 13
+
+-- 14.
+-- Donnez, par année, le nombre d’albums édité cette année-là ainsi que le prix moyen de ces
+-- albums. Les années où aucun album n’a été édité ne doivent pas apparaître.
+-- TODO 14
+
+-- 15.
+-- Donnez les dessinateurs qui ont travaillé sur des albums de plusieurs séries.
+-- TODO 15
+
+-- 16.
+-- Donnez la date d'édition la plus ancienne parmi les albums édités chez "Dargaud".
+-- TODO 16
+
+-- 17.
+-- Donnez le(s) albums le(s) plus ancien(s) parmi ceux édités chez "Dargaud".
+-- TODO 17
+
+-- 18.
+-- Donnez, pour chaque album, édité en Belgique ou en France, ayant au moins un auteur répertorié,
+-- son isbn, son titre et le nombre d’auteurs intervenant dans cet album. Classez les albums par ordre
+-- décroissant du nombre d’auteurs.
+-- TODO 18
+
+-- 19.
+-- Donnez, pour chaque paire d’albums de même titre, les isbn, date d’édition et le titre.
+-- TODO 19
+
+-- 20.
+-- Donnez, pour chaque auteur dont l’adresse e-mail contient «@yahoo» et ayant participé à
+-- plusieurs albums, son identifiant, son nom, son adresse e-mail, son nombre d’albums et le nombre
+-- d’éditeurs ayant publié au moins un de ses albums.
+-- TODO 20
+
+-- 21.
+-- Pour chaque auteur, donnez son identifiant, son nom et le nombre d’albums pour lesquels il est
+-- le seul auteur. Il ne faut pas prendre les auteurs n’ayant aucun album pour lequel il est le seul
+-- auteur.
+-- TODO 21
